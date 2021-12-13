bb9c6a14948a59895d3f35e3c6b97781b5b7b8ad
VictorMadrona <victormadrona@uma.es>
2021-12-13 12:14:23 +0100

    main

--- src/Ejercicio1/Main.java
+++ src/Ejercicio1/Main.java
@@ -1,4 +1,13 @@
 package Ejercicio1;
 
+import java.util.Date;
+
 public class Main {
+    Profesional p1 = new Profesional();
+    Expediente e1 = new Expediente();
+    Date fecha = new Date();
+    TipoAcceso tipo = TipoAcceso.CONSULTA;
+
+    Acceso a1 = new Acceso(fecha,tipo,p1,e1);
+
 }
