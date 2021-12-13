ed9b81294da8a8e9469cbe2f40fe6b78687db412
pablogodiaz <pablogodiaz+1@gmail.com>
2021-12-13 11:51:19 +0100

    Update Profesional.java

--- src/Ejercicio1/Profesional.java
+++ src/Ejercicio1/Profesional.java
@@ -1,8 +1,27 @@
 package Ejercicio1;
 
+import java.util.List;
+
 public class Profesional {
     private List<Acceso> accesores;
-    public Profesional() {
 
+    public Profesional() {
+        this.accesores = null;
+    }
+
+    public Profesional(List<Acceso> acc) {
+        this.accesores = acc;
+    }
+
+    public List<Acceso> getAccesores() {
+        return this.accesores;
+    }
+
+    public void setAccesores(List<Acceso> acc) {
+        this.accesores = acc;
+    }
+
+    public void addAcceso(Acceso a) {
+        accesores.add(a);
     }
 }
