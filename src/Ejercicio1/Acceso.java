432a68992ed1ac6fe3900992c2012ce6bacd24f4
VictorMadrona <victormadrona@uma.es>
2021-12-13 11:38:08 +0100

    constructor acceso

--- src/Ejercicio1/Acceso.java
+++ src/Ejercicio1/Acceso.java
@@ -5,4 +5,9 @@ import java.util.Date;
 public class Acceso {
     public Date fecha;
     public TipoAcceso tipo;
+
+    public Acceso(Date fecha, TipoAcceso tipo){
+        this.fecha = fecha;
+        this.tipo = tipo;
+    }
 }
