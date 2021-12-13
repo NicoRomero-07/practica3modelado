b9ae2c7d22704060aec42fa647b12347adc1f1f8
Alfonso <alfonsogonzalezortiz@uma.es>
2021-12-13 12:02:58 +0100

    Clase Expediente

--- src/Ejercicio1/Expediente.java
+++ src/Ejercicio1/Expediente.java
@@ -1,4 +1,36 @@
 package Ejercicio1;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Expediente {
+    private Paciente paciente;
+    private List<Acceso> acceso;
+
+    public Expediente(){
+        this.paciente = null;
+        this.acceso = new ArrayList<>();
+    }
+
+    public Expediente(Paciente paciente,List<Acceso> acceso){
+        this.paciente = paciente;
+        this.acceso = acceso;
+    }
+
+    public Paciente getPaciente(){
+        return paciente;
+    }
+
+    public List<Acceso> getAcceso(){
+        return acceso;
+    }
+
+    protected void addAcceso(Acceso a){
+        acceso.add(a);
+    }
+
+    protected void removeAcceso(Acceso a){
+        acceso.remove(a);
+    }
+
 }
