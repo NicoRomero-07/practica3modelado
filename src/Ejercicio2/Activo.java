eda099f49532a54be30ba910c86dd4e865f18557
Alfonso <alfonsogonzalezortiz@uma.es>
2021-12-19 12:25:57 +0100

    Clase Activo Arreglado

--- src/Ejercicio2/Activo.java
+++ src/Ejercicio2/Activo.java
@@ -1,6 +1,12 @@
 package Ejercicio2;
 
-public class Activo implements Trabajador{
+import Ejercicio1.Acceso;
+
+public class Activo extends Trabajador {
+
+    public Activo(String nombre,String ss){
+        super(nombre,ss);
+    }
 
     @Override
     protected void incrementar() {
