5a9d8f6a2624f5943690105c5bcca05678c7e97d
VictorMadrona <victormadrona@uma.es>
2021-12-19 13:13:53 +0100

    main

--- src/Ejercicio2/MedioPensionista.java
+++ src/Ejercicio2/MedioPensionista.java
@@ -1,18 +1,23 @@
 package Ejercicio2;
 
-public class MedioPensionista {
+public class MedioPensionista implements ActivoInterface, PensionistaInterface{
 
     private Activo activo;
     private Pensionista pensionista;
     private double salario;
 
 
+    public MedioPensionista(String nombre, String ss){
+        activo = new Activo(nombre,ss);
+        pensionista = new Pensionista(nombre,ss);
+    }
+
     public MedioPensionista(Activo a, Pensionista p){
         activo = a;
         pensionista = p;
     }
 
-    protected void incrementar(){
+    public void incrementar(){
         activo.incrementar();
         pensionista.incrementar();
         salario = activo.getSalario() + pensionista.getSalario();
