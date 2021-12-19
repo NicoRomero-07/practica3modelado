dc7a20bdf6652d5ce0d8a8b4dcf34b53a87e375b
Alfonso <alfonsogonzalezortiz@uma.es>
2021-12-19 13:56:06 +0100

    Ejercicio 2 completo v2

--- src/Ejercicio2/Main.java
+++ src/Ejercicio2/Main.java
@@ -1,14 +1,12 @@
 package Ejercicio2;
 
-import javax.sound.sampled.SourceDataLine;
-
 public class Main {
     public static void main (String[] args){
         try{
             MedioPensionista mp = new MedioPensionista(new Activo("Paco","1",2.0), new Pensionista("Paco","1",2.0));
             mp.incrementar();
             System.out.println(mp.getSalario());
-        }catch (Exception e){
+        }catch (Exception ignored){
 
         }
     }
