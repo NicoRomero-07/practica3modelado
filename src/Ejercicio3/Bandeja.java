7080d5766e970da9cfd3c82aabd5d6551b85c2ff
VictorMadrona <victormadrona@uma.es>
2022-01-09 20:06:21 +0100

    main

--- src/Ejercicio3/Bandeja.java
+++ src/Ejercicio3/Bandeja.java
@@ -16,7 +16,7 @@ public class Bandeja {
         this.piezas = new LinkedList<Pieza>();
     }
 
-    public Bandeja(Integer capacidad, SortedSet<Pieza> piezas){
+    public Bandeja(Integer capacidad, LinkedList<Pieza> piezas){
         assert capacidad > 0 : "La capacidad no puede ser menor que cero";
         assert capacidad >= piezas.size() : "La capacidad debe ser mayor al tamano de la lista de piezas";
         state = new EmptyState(this);
@@ -36,7 +36,7 @@ public class Bandeja {
         return this.capacidad;
     }
 
-    protected SortedSet<Pieza> getPiezas() {
+    protected LinkedList<Pieza> getPiezas() {
         return this.piezas;
     }
 
@@ -47,7 +47,7 @@ public class Bandeja {
     }
 
     protected Pieza get() {
-        Pieza p = piezas.last();
+        Pieza p = (Pieza) piezas.getLast();
         piezas.remove(p);
         state.onGet();
         return p;
